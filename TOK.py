4d1a58debb91fc8dd98995586afe242283f0f96c
Jack Woodrow <woodrowjack@gmail.com>
2014-12-07 15:21:51 -0500

    alpha version
    Mostly final code, just some optimizing and fixing before beta

--- TOK.py
+++ TOK.py
@@ -1,26 +1,20 @@
 import pyglet
-
 from pyglet.gl import *
-
-import math
-
-import random
-
-from pyglet import clock
-
-window = pyglet.window.Window(resizable=True)
-
 from pyglet.window import key
 from pyglet.window import Window
+import math
+import random
 
-@window.event
+window = pyglet.window.Window(caption='TOK GAME', resizable=True) 
 
-def circle(x, y, radius):
+pyglet.gl.glClearColor(1, 1, 1, 1)
+
+space = 0
+
+def circle(x, y, radius, g, b):
     iterations = int(2*radius*math.pi)
     s = math.sin(2*math.pi / iterations)
     c = math.cos(2*math.pi / iterations)
-    g = random.uniform(0.1,1)
-    b = random.uniform(0.1,1)
     dx, dy = radius, 0
  
     glBegin(GL_TRIANGLE_FAN)
@@ -30,10 +24,27 @@ def circle(x, y, radius):
         glVertex2f(x+dx, y+dy)
         dx, dy = (dx*c - dy*s), (dy*c + dx*s)
     glEnd()
+
+@window.event
+def on_resize(width, height):
+    window.set_size(window.height, window.height)
+    window.clear()
+    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,  ('v2f', ((window.width//2.1),(window.height//2),(window.width//1.9),(window.height//2))), ('c3i', (0,0,0)*2))
+    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,  ('v2f', ((window.width//2),(window.height//2.1),(window.width//2),(window.height//1.9))), ('c3i', (0,0,0)*2))
+
 @window.event
 def on_key_press(symbol, modifiers):
+    global space
+    g = random.uniform(0.1,1)
+    b = random.uniform(0.1,1)
     if symbol == key.BACKSPACE:
         window.clear()
     if symbol == key.SPACE:
-        circle(window.width//2,window.height//2,(window.height//12)//2)
+        space = space+1
+        print space
+        for a in xrange (0,360,30):
+            circle((math.sin(math.radians(a))*(window.width//2.5))+(window.width//2),(math.cos(math.radians(a))*(window.height//2.5))+(window.height//2),window.height//12,g,b)
+        r = random.randrange(0,360,30)
+        print r,'degrees'
+        circle((math.sin(math.radians(r))*(window.width//2.5))+(window.width//2),(math.cos(math.radians(r))*(window.height//2.5))+(window.height//2),window.height//12,(g+(1-(random.gauss(0,1)))),(b+(1-(random.gauss(0,1)))))
 pyglet.app.run()
