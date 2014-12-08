4733fa08c4653b70c5b2d272875b0351d36a4812
Jack Woodrow <woodrowjack@gmail.com>
2014-12-07 19:52:51 -0500

    Fixed resize issues, allowed non square window
    Read me also updated with instructions

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ TOK-colour-game
 ===============
 
 Python colour recognition game
+
+SPACE = New colors
+BACKSPACE = Clear All
+F = Full screen
--- TOK.py
+++ TOK.py
@@ -5,14 +5,14 @@ from pyglet.window import Window
 import math
 import random
 
-window = pyglet.window.Window(caption='TOK GAME', resizable=True) 
+window = pyglet.window.Window(caption='TOK GAME', resizable=True)
 
 pyglet.gl.glClearColor(1, 1, 1, 1)
 
 space = 0
 
 def circle(x, y, radius, g, b):
-    iterations = int(2*radius*math.pi)
+    iterations = 8
     s = math.sin(2*math.pi / iterations)
     c = math.cos(2*math.pi / iterations)
     dx, dy = radius, 0
@@ -24,27 +24,34 @@ def circle(x, y, radius, g, b):
         glVertex2f(x+dx, y+dy)
         dx, dy = (dx*c - dy*s), (dy*c + dx*s)
     glEnd()
+    
+@window.event
+def on_activate():
+    window.clear()
 
 @window.event
 def on_resize(width, height):
-    window.set_size(window.height, window.height)
     window.clear()
+@window.event
+def on_draw():
     pyglet.graphics.draw(2, pyglet.gl.GL_LINES,  ('v2f', ((window.width//2.1),(window.height//2),(window.width//1.9),(window.height//2))), ('c3i', (0,0,0)*2))
-    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,  ('v2f', ((window.width//2),(window.height//2.1),(window.width//2),(window.height//1.9))), ('c3i', (0,0,0)*2))
+    pyglet.graphics.draw(2, pyglet.gl.GL_LINES,  ('v2f', ((window.width//2),((window.height//2)+(window.width//40)),(window.width//2),((window.height//2)-(window.width//40)))), ('c3i', (0,0,0)*2))
 
 @window.event
 def on_key_press(symbol, modifiers):
+    window.clear()
     global space
     g = random.uniform(0.1,1)
     b = random.uniform(0.1,1)
+    if symbol == key.F:
+        window.set_fullscreen(fullscreen=True, screen=None)
     if symbol == key.BACKSPACE:
         window.clear()
     if symbol == key.SPACE:
         space = space+1
-        print space
         for a in xrange (0,360,30):
-            circle((math.sin(math.radians(a))*(window.width//2.5))+(window.width//2),(math.cos(math.radians(a))*(window.height//2.5))+(window.height//2),window.height//12,g,b)
+            circle((math.sin(math.radians(a))*(window.height//2.5))+(window.width//2),(math.cos(math.radians(a))*(window.height//2.5))+(window.height//2),window.height//12,g,b)
         r = random.randrange(0,360,30)
         print r,'degrees'
-        circle((math.sin(math.radians(r))*(window.width//2.5))+(window.width//2),(math.cos(math.radians(r))*(window.height//2.5))+(window.height//2),window.height//12,(g+(1-(random.gauss(0,1)))),(b+(1-(random.gauss(0,1)))))
+        circle((math.sin(math.radians(r))*(window.height//2.5))+(window.width//2),(math.cos(math.radians(r))*(window.height//2.5))+(window.height//2),window.height//12,(g+(1-(random.gauss(0,1)))),(b+(1-(random.gauss(0,1)))))
 pyglet.app.run()
